8f64615c1831ca2a9e6e81a99309cb754e786464
mitulpatel224 <mitul.patel224@gmail.com>
2023-05-19 12:33:20 +0530

    feat: pie chart

--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardComponent } from '../../card/card.component';
+import { PieChartComponent } from '../../shared/pie-chart/pie-chart.component';
 import { HealthComponent } from '../health/health.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CardComponent, HealthComponent],
+  imports: [CommonModule, CardComponent, HealthComponent, PieChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
