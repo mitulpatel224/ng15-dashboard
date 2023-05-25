29d9e04c475619730c88188932fa84df8cb7bf09
mitulpatel224 <mitul.patel224@gmail.com>
2023-05-25 20:29:42 +0530

    feat: dynamic theme
    Added theme based on windows preferred theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { Chart, registerables } from 'chart.js';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { NgChartsModule } from 'ng2-charts';
+// -------------------------------------------------
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/core/components/sidebar/sidebar.component';
 import { TopbarComponent } from './components/core/components/topbar/topbar.component';
-Chart.register(...registerables, ChartDataLabels);
-Chart.defaults.plugins.legend.labels.usePointStyle = true;
-Chart.defaults.plugins.legend.labels.pointStyle = 'circle';
-Chart.defaults.plugins.legend.labels.padding = 20;
-Chart.defaults.plugins.legend.align = 'start';
-Chart.defaults.color = '#cdcdcd';
-Chart.defaults.maintainAspectRatio = false;
-Chart.defaults.aspectRatio = 1;
-Chart.defaults.responsive = true;
-Chart.defaults.font.size = 14;
-Chart.defaults.scale.grid.color = '#222533';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
--- src/app/components/core/components/sidebar/sidebar.component.ts
+++ src/app/components/core/components/sidebar/sidebar.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, Renderer2 } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  imports: [CommonModule, FormsModule],
+  standalone: true,
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnDestroy {
+  /** Persist the active theme mode */
+  public isDarkMode: boolean = true;
 
+  constructor(private renderer: Renderer2) {
+    /** Fetch the active theme mode and set the dark-mode flag */
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: light)').matches
+    ) {
+      this.isDarkMode = false;
+      this.toggleTheme(this.isDarkMode);
+    }
+
+    /** Watch over window's theme change */
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .addEventListener('change', this.handleThemeChange);
+  }
+
+  /**
+   * Handle toggle switch for theme change
+   * @param mode boolean
+   */
+  public toggleTheme(mode: boolean) {
+    const body = document.querySelector('body') as any;
+    switch (mode) {
+      case true:
+        this.renderer.setAttribute(body, 'data-theme', 'dark');
+        break;
+
+      default:
+        this.renderer.setAttribute(body, 'data-theme', 'light');
+        break;
+    }
+  }
+
+  /**
+   * Handle windows theme change
+   * @param event Theme change event
+   */
+  public handleThemeChange = (event: any) => {
+    this.isDarkMode = event.matches ? true : false;
+    this.toggleTheme(this.isDarkMode);
+    this.ngOnDestroy();
+  };
+
+  ngOnDestroy(): void {
+    window
+      .matchMedia('(prefers-color-scheme: dark)')
+      .removeEventListener('change', this.handleThemeChange, true);
+  }
 }
